1d725672748097f500f33d238e85b61ebb9b37bb
William Corrêa de Oliveira Stella <willcstella@gmail.com>
2024-11-12 23:07:35 -0300

    Realização do exercicio debug

--- funcoes_novas.py
+++ funcoes_novas.py
@@ -1,1 +1,13 @@
 print("novas funções")
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
